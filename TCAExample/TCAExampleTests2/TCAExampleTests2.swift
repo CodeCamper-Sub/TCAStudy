f343292fed7a7520aec87a06a450ae002f80879d
Eric/한경수 <gksrudtn99@gmail.com>
2024-07-31 11:47:32 +0900

    Testing your feature
    https://pointfreeco.github.io/swift-composable-architecture/main/tutorials/composablearchitecture/01-03-testingyourfeature

---
+++ TCAExample/TCAExampleTests2/TCAExampleTests2.swift
@@ -0,0 +1,64 @@
+//
+//  TCAExampleTests.swift
+//  TCAExampleTests
+//
+//  Created by 한경수 on 7/31/24.
+//
+
+import XCTest
+import ComposableArchitecture
+
+@testable import TCAExample
+
+@MainActor
+final class CounterFeatureTest: XCTestCase {
+  func testCounter() async {
+    let store = TestStore(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+    
+    await store.send(.incrementButtonTapped) {
+      $0.count = 1
+    }
+    await store.send(.decrementButtonTapped) {
+      $0.count = 0
+    }
+  }
+  
+  func testTimer() async {
+    let clock = TestClock()
+    
+    let store = TestStore(initialState: CounterFeature.State()) {
+      CounterFeature()
+    } withDependencies: {
+      $0.continuousClock = clock
+    }
+    
+    await store.send(.toggleTimerButtonTapped) {
+      $0.isTimerRunning = true
+    }
+    await clock.advance(by: .seconds(1))
+    await store.receive(\.timerTick) {
+      $0.count = 1
+    }
+    await store.send(.toggleTimerButtonTapped) {
+      $0.isTimerRunning = false
+    }
+  }
+  
+  func testNumberFact() async {
+    let store = TestStore(initialState: CounterFeature.State()) {
+      CounterFeature()
+    } withDependencies: {
+      $0.numberFact.fetch = { "\($0) is a good number" }
+    }
+    
+    await store.send(.factButtonTapped) {
+      $0.isLoading = true
+    }
+    await store.receive(\.factResponse) {
+      $0.isLoading = false
+      $0.fact = "0 is a good number"
+    }
+  }
+}
